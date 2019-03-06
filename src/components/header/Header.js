46143e823f5d34451b03fe6d4c094aaefb795cbd
MikeyZat <mikeyzat@gmail.com>
2019-03-06 10:32:22 +0100

    updated header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,29 +1,31 @@
 import React from 'react';
 import '../../style/css/Header.minx.css';
 
-const Header = ({english}) =>{
-    return(<header>
-        <div className={"smoke-container"}>
-            <video src="smoke.mp4" autoPlay muted> </video>
-            <h1>
-                <span>M</span>
-                <span>i</span>
-                <span>k</span>
-                <span>o</span>
-                <span>ł</span>
-                <span>a</span>
-                <span>j</span>
-                <br/>
-                <span>Z</span>
-                <span>a</span>
-                <span>t</span>
-                <span>o</span>
-                <span>r</span>
-                <span>s</span>
-                <span>k</span>
-                <span>i</span>
-            </h1>
-        </div>
+const Header = ({english}) => {
+    return (<header className={"smoke-container"}>
+        <video src="smoke.mp4" autoPlay muted></video>
+        <h1>
+            <span>M</span>
+            <span>i</span>
+            <span>k</span>
+            <span>o</span>
+            <span>ł</span>
+            <span>a</span>
+            <span>j</span>
+            <br/>
+            <span>Z</span>
+            <span>a</span>
+            <span>t</span>
+            <span>o</span>
+            <span>r</span>
+            <span>s</span>
+            <span>k</span>
+            <span>i</span>
+        </h1>
+        <p className={"intro"}>
+            {!english ? "Aspirujący na web developera student Informatyki." : "Aspiring web developer & Computer Science student."}
+        </p>
+        <a href={"#about_me"} className={"start"}><span className={"arrow"}> </span></a>
     </header>)
 };
 
