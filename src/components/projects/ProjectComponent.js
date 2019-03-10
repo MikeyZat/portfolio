638bd637bb0ee376661d25e55addf3d21168bfae
MikeyZat <mikeyzat@gmail.com>
2019-03-10 18:45:55 +0100

    project section for mobile finished

--- src/components/projects/ProjectComponent.js
+++ src/components/projects/ProjectComponent.js
@@ -7,10 +7,17 @@ const ProjectComponent = ({english, imgSrc, name, liveLink, githubLink}) => {
         <div className={"img-container"}>
             <img src={imgSrc} alt={name} className={"project-img"}/>
         </div>
-        <span className={"prohect-title"}>{name}</span>
-        <a href={liveLink} target="_blank" rel="noopener noreferrer" className={"live-link"}>Live</a>
-        <a href={githubLink} target="_blank" rel="noopener noreferrer"
-           className={"code-link"}>{!english ? "Kod" : "Code"}</a>
+        <div className={"info-container"}>
+            <span className={"project-title"}><span className={"title"}>{name}</span></span>
+            <a href={liveLink} target="_blank" rel="noopener noreferrer" className={"live-link"}>
+                <span className={"text"}>Live</span>
+            </a>
+            <a href={githubLink} target="_blank" rel="noopener noreferrer"
+               className={"code-link"}>
+                <i className="fab fa-github"/>
+                <span className={"text"}>{!english ? "Kod" : "Code"}</span>
+            </a>
+        </div>
     </article>
 };
 
