e2e89ac7ec1e88b9799af6c8f540c30b0c2ebf18
MikeyZat <mikeyzat@gmail.com>
2019-03-11 12:05:58 +0100

    project section for desktop finished

--- src/components/projects/ProjectComponent.js
+++ src/components/projects/ProjectComponent.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import '../../style/css/Projects.minx.css';
 
-const ProjectComponent = ({english, imgSrc, name, liveLink, githubLink}) => {
+const ProjectComponent = ({english, imgSrc, name, liveLink, githubLink, right}) => {
 
     return <article className={"project"}>
         <div className={"img-container"}>
             <img src={imgSrc} alt={name} className={"project-img"}/>
         </div>
-        <div className={"info-container"}>
+        <div className={"info-container " + (right ? "right" : "left")}>
             <span className={"project-title"}><span className={"title"}>{name}</span></span>
             <a href={liveLink} target="_blank" rel="noopener noreferrer" className={"live-link"}>
                 <span className={"text"}>Live</span>
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -2,36 +2,41 @@ import React from 'react';
 import '../../style/css/Projects.minx.css';
 import ProjectComponent from "./ProjectComponent";
 
-const Projects = ({english}) =>{
+const Projects = ({english}) => {
 
     return <section id={"projects"} className={"projects-section"}>
-        <h2>{!english?"Projekty":"Projects"}</h2>
+        <h2>{!english ? "Projekty" : "Projects"}</h2>
         <div className={"projects-container"}>
             <ProjectComponent english={english}
                               name={"flaneczki.pl"}
                               imgSrc={"flaneczki.png"}
                               githubLink={""}
-                              liveLink={"https://flaneczki.pl/"}/>
+                              liveLink={"https://flaneczki.pl/"}
+                              right={true}/>
             <ProjectComponent english={english}
                               name={"Portfolio"}
                               imgSrc={"portfolio.png"}
                               githubLink={"https://github.com/MikeyZat/portfolio"}
-                              liveLink={"https://mikeyzat.github.io/"}/>
+                              liveLink={"https://mikeyzat.github.io/"}
+                              right={true}/>
             <ProjectComponent english={english}
                               name={"Air Condition App"}
                               imgSrc={"airCondition.png"}
                               githubLink={"https://github.com/MikeyZat/AirCondition"}
-                              liveLink={"https://mikeyzat.github.io/AirCondition/"}/>
+                              liveLink={"https://mikeyzat.github.io/AirCondition/"}
+                              right={false}/>
             <ProjectComponent english={english}
                               name={"Paypal Invoice Calculator"}
                               imgSrc={"priceConverter.png"}
                               githubLink={"https://github.com/MikeyZat/PriceConverted"}
-                              liveLink={"https://mikeyzat.github.io/PriceConverted/"}/>
+                              liveLink={"https://mikeyzat.github.io/PriceConverted/"}
+                              right={false}/>
             <ProjectComponent english={english}
                               name={"To Do List"}
                               imgSrc={"notes.png"}
                               githubLink={"https://github.com/MikeyZat/bestNotes"}
-                              liveLink={"https://mikeyzat.github.io/bestNotes/"}/>
+                              liveLink={"https://mikeyzat.github.io/bestNotes/"}
+                              right={false}/>
         </div>
 
     </section>
