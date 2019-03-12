0c7cc1b977a57af4aac7cc0c9ae15231c1b394bf
MikeyZat <mikeyzat@gmail.com>
2019-03-12 15:30:11 +0100

    finished career section

--- src/components/careerPath/CareerPath.js
+++ src/components/careerPath/CareerPath.js
@@ -5,34 +5,34 @@ const CareerPath = ({english}) => {
 
     return (<section id={"career"} className={"career-section"}>
         <h2>{!english ? "Ścieżka kariery" : "Career path"}</h2>
-            <div className={"timeline-box"}>
-                <ul className={"timeline"}>
-                    <li>
-                        <div className={"item"}>
-                            <span className={"date"}>3rd January 2017</span>
-                            <h3 className={"text"}>Bla bla bla</h3>
-                        </div>
-                    </li>
-                    <li>
-                        <div className={"item"}>
-                            <span className={"date"}>30st January 2017</span>
-                            <h3 className={"text"}>Bla bla bla</h3>
-                        </div>
-                    </li>
-                    <li>
-                        <div className={"item"}>
-                            <span className={"date"}>21 February 2017</span>
-                            <h3 className={"text"}>Bla bla bla</h3>
-                        </div>
-                    </li>
-                    <li>
-                        <div className={"item"}>
-                            <span className={"date"}>1st August 2017</span>
-                            <h3 className={"text"}>Bla bla bla</h3>
-                        </div>
-                    </li>
-                </ul>
-            </div>
+        <div className={"timeline-box"}>
+            <ul className={"timeline"}>
+                <li>
+                    <div className={"item"}>
+                        <span className={"date"}>09.2017</span>
+                        <h3 className={"text"}>{!english ? "Pierwszy kontakt z programowaniem" : "First contact with programming"}</h3>
+                    </div>
+                </li>
+                <li>
+                    <div className={"item"}>
+                        <span className={"date"}>06.2018</span>
+                        <h3 className={"text"}>{!english ? "Nauka Javy i pierwsze własne projekty" : "Learning java and creating first own projects"}</h3>
+                    </div>
+                </li>
+                <li>
+                    <div className={"item"}>
+                        <span className={"date"}>10.2018</span>
+                        <h3 className={"text"}>{!english ? "Początek nauki JavaScript'u i biblioteki React.js" : "Started to learn JavaScript and React.js"}</h3>
+                    </div>
+                </li>
+                <li>
+                    <div className={"item"}>
+                        <span className={"date"}>03.2019</span>
+                        <h3 className={"text"}>{!english ? "Opublikowanie największego dotychczas projektu : flaneczki.pl" : "Publishing the greatest project so far: flaneczki.pl"}</h3>
+                    </div>
+                </li>
+            </ul>
+        </div>
     </section>);
 };
 
